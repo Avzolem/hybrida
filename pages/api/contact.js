b60609b80c144ca3f8b1647b295a2a8fc93fc5f6
Andres Aguilar <aaguilarc@uach.mx>
2023-01-29 01:22:51 -0600

    Sending Email using nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,6 +3,7 @@ import nc from "next-connect";
 import ncoptions from "@/config/ncoptions";
 import axios from "axios";
 import FormData from "form-data";
+const nodemailer = require("nodemailer");
 
 const handler = nc(ncoptions);
 
@@ -13,26 +14,54 @@ handler.use(async (req, res, next) => {
 
 handler.post(async (req, res) => {
     const { data } = req.body;
+
     const { firstName, lastName, email, phone, company, message } = data;
     console.log(data);
 
     const parsedData = {
-        name: firstName,
-        // lastName,
+        firstName,
+        lastName,
         email,
-        // phone,
-        // company,
+        phone,
+        company,
         message,
     };
 
     try {
-        //convert parsedData to form data
-        const formData = new FormData();
-        for (const key in parsedData) {
-            formData.append(key, parsedData[key]);
-        }
-
         //send parsedData to email
+        let transporter = nodemailer.createTransport({
+            host: process.env.SMTP_HOST,
+            port: 587,
+            secure: false, // true for 465, false for other ports
+            auth: {
+                user: process.env.SMTP_USER,
+                pass: process.env.SMTP_PASS,
+            },
+        });
+
+        const template = `
+          <p><strong>Nombre</strong>: ${firstName}</p>
+          </br>  
+          <p><strong>Apellido</strong>: ${lastName}</p>
+          </br>  
+          <p><strong>Email</strong>: ${email}</p>
+          </br>  
+          <p><strong>Telefono</strong>: ${phone}</p>
+          </br>  
+          <p><strong>Escuela/Organizacion</strong>: ${company}</p>
+          </br>  
+          <p><strong>Mensaje</strong>: ${message}</p>
+          </br> 
+        `;
+
+        let info = await transporter.sendMail({
+            from: "hybrida@uach.mx", // sender address
+            to: "hybrida@uach.mx", // list of receivers
+            subject: "Nuevo Registro Hybrida Uach ✔", // Subject line
+            html: template,
+        });
+
+        console.log("Message sent: %s", info.messageId);
 
         res.status(200).json({ message: "success" });
         //send parsedData to google sheets
