75c768d69e51acfa8cc5d078e1c67e4e1d84c488
Andres Aguilar <aaguilarc@uach.mx>
2023-02-20 14:33:10 -0600

    Merge timeline

--- pages/timeline/index.js
+++ pages/timeline/index.js
@@ -10,14 +10,6 @@ export default function Home() {
                 title="artistas"
                 description="Cronograma de actividades"
             >
-                {/* <!-- Timeline component --> */}
-                <link
-                    rel="stylesheet"
-                    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css"
-                    integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog=="
-                    crossorigin="anonymous"
-                />
-
                 <div class="relative min-h-screen">
                     <div class="flex min-h-screen justify-center">
                         <div class="mx-auto w-2/3">
