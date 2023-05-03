80607fe5b7f363c613dbbdf5e6c102a44024f000
Andres Aguilar <aaguilarc@uach.mx>
2023-05-03 08:54:21 -0600

    Cambio de url

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ module.exports = {
             {
                 source: "/workshop",
                 destination:
-                    "https://www.notion.so/15329b1441d34f92ac5a44548ed9950d?v=7494f390fea64f72b9a04e073d261b79",
+                    "https://cotton-lung-cdb.notion.site/15329b1441d34f92ac5a44548ed9950d?v=7494f390fea64f72b9a04e073d261b79",
                 permanent: false,
             },
         ];
--- pages/workshop.js
+++ pages/workshop.js
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 export default function Workshop() {
     useEffect(() => {
         window.location.href =
-            "https://www.notion.so/15329b1441d34f92ac5a44548ed9950d?v=7494f390fea64f72b9a04e073d261b79";
+            "https://cotton-lung-cdb.notion.site/15329b1441d34f92ac5a44548ed9950d?v=7494f390fea64f72b9a04e073d261b79";
     }, []);
 
     return (
